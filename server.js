92b9a474791b4c008908ba1aa392e47d5c2f0a92
Jongho Bae <jb4918@columbia.edu>
2024-07-04 23:25:26 -0400

    Added get \notes

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname + '/public/index.html'))
 );
 
+app.get("/notes", (req, res) => 
+    res.sendFile(path.join(__dirname + '/public/notes.html'))
+);
+
+
 // When clicking start button from landing page
 app.get('/api/notes', (req, res) => {
     // Read notes from the json file
